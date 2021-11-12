4fcadd3ccb61739459528f3afb1f6de7ca4d9cc2
KesavanG <kesavang4020@gmail.com>
2021-11-12 18:15:50 +0530

    Revert "violet: Add GoogleCameraGo"
    This reverts commit 2ea61d81e57575a1561674618c2468f3b71846b6.

--- device.mk
+++ device.mk
@@ -149,7 +149,7 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor \
     libgui_vendor \
     libxml2 \
-    GoogleCameraGo \
+    Camera2 \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 PRODUCT_COPY_FILES += \
