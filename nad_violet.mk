cf2686707567239209111be61df10992ab3a02ad
KesavanG <kesavang4020@gmail.com>
2021-11-12 18:28:43 +0530

    violet:redfin Fingerprint

--- nad_violet.mk
+++ nad_violet.mk
@@ -27,4 +27,15 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
+# Fingerprint
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys
+BUILD_DESCRIPTION := redfin-user 11 RQ2A.210505.003 7255357 release-keys
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="$(BUILD_DESCRIPTION)" \
+    PRODUCT_NAME="violet"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
